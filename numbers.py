74c49f5d8d5937c9775e7dcf29da1ffcc167dd9b
chehpvskiy01@bk.ru <chehovskiy01@bk.ru>
2025-10-26 23:43:36 +0300

    Add numbers.py

---
+++ numbers.py
@@ -0,0 +1,11 @@
+a=input("Введите a = ")
+b=input("Введите b = ")
+c=input("Введите c = ")
+d=input("Введите d = ")
+
+ab=a+b
+cd=c+d
+print("ab = "+str(ab))
+print("cd = "+str(cd))
+result=int(ab)/int(cd)
+print("Результат ab/cd= "+str(result))
